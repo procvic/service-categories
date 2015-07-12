3d0c30bb4d9a782caf1a596082f271d8d75a5f45
Václav Makeš <info@makes.cz>
2015-07-12 11:01:34 +0200

    Draft for structure of categories and subjects

--- src/subjects.php
+++ src/subjects.php
@@ -1,13 +1,25 @@
 <?php
 
 $result = [
-    1 => "Matematika pro 1. stupeň ZŠ",
-    2 => "Matematika pro 2. stupeň ZŠ",
-    3 => "Matematika pro SŠ",
-    4 => "Matematika pro VŠ",
+    1 => [
+        'name' => 'Matematika',
+        'subcategories' => [
+            2 => '1. stupeň ZŠ',
+            3 => '2. stupeň ZŠ',
+            4 => 'SŠ',
+            5 => 'VŠ',
+        ],
+    ],
+    6 => [
+        'name' => 'Dějepis',
+        'subcategories' => [
+            7 => 'ZŠ',
+            8 => 'SŠ',
+        ],
+    ],
 ];
 
-header("Content-Type: text/javascript");
+header('Content-Type: text/javascript');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 echo json_encode($result);
